6e681c8cc82d36e206f7e4fcf525aca1729b6064
Marianna Bisyarina <bisyarinamariashka@gmail.com>
2014-03-17 00:48:06 +0400

    Initial commit

---
+++ src/lib/io/Scanner.java
@@ -0,0 +1,68 @@
+package lib.io;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.InputMismatchException;
+import java.util.StringTokenizer;
+
+/**
+ * Fast Scanner.
+ */
+@SuppressWarnings("unused")
+public class Scanner {
+    BufferedReader in;
+    StringTokenizer tok;
+
+    public Scanner(InputStream in) {
+        this.in = new BufferedReader(new InputStreamReader(in));
+        tok = new StringTokenizer("");
+    }
+
+    public String nextToken() {
+        while (!tok.hasMoreTokens()) {
+            tok = new StringTokenizer(next());
+        }
+
+        return tok.nextToken();
+    }
+
+    private String tryReadNextLine() {
+        try {
+            return in.readLine();
+        } catch (IOException e) {
+            throw new InputMismatchException();
+        }
+    }
+
+    public String next() {
+        String newLine = tryReadNextLine();
+        if (newLine == null)
+            throw new InputMismatchException();
+        return newLine;
+    }
+
+    public int nextInt() {
+        return Integer.parseInt(nextToken());
+    }
+
+    public long nextLong() {
+        return Long.parseLong(nextToken());
+    }
+
+    public double nextDouble() {
+        return Double.parseDouble(nextToken());
+    }
+
+    public boolean EOF() {
+        while (!tok.hasMoreTokens()) {
+            String newLine = tryReadNextLine();
+            if (newLine == null)
+                return true;
+            tok = new StringTokenizer(newLine);
+        }
+        return false;
+    }
+
+}
